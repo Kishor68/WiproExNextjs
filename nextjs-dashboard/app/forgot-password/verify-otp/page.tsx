a38f2c15eb105c1d16cb5f80af046417663568e9
Kishor68 <163232904+Kishor68@users.noreply.github.com>
2026-05-22 11:44:48 +0530

    fix: include verify otp reset page in latest push

--- nextjs-dashboard/app/forgot-password/verify-otp/page.tsx
+++ nextjs-dashboard/app/forgot-password/verify-otp/page.tsx
@@ -1,6 +1,7 @@
 import VerifyOTPForm from '@/app/ui/verify-otp-form';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Verify OTP',
@@ -15,7 +16,9 @@ export default function VerifyOTPPage() {
             <AcmeLogo />
           </div>
         </div>
+        <Suspense fallback={<div className="text-center text-sm text-gray-500">Loading...</div>}>
         <VerifyOTPForm />
+          </Suspense>
       </div>
     </main>
   );
